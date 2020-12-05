85ed074870814a4358d58d0370db0126a5616f88
kalanasa1996s <kalanasa1996@gmail.com>
2020-12-05 14:45:28 +0530

    create project

---
+++ src/main/java/lk/ijse/instagram/instagram/InstagramApplication.java
@@ -0,0 +1,14 @@
+package lk.ijse.instagram.instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hi");
+	}
+
+}
